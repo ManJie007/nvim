e4ec21e3b5a821602c2977a48e045d1b4f5bd6a7
MANJIE007 <297867870@qq.com>
2024-10-29 14:06:30 +0800

    add treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,3 +20,20 @@ if nvim_tree then
       },
     })
 end
+
+-- nvim-treesitter
+local nvim_treesitter_configs = safe_require('nvim-treesitter.configs')
+if nvim_treesitter_configs then
+    nvim_treesitter_configs.setup({
+        ensure_installed = { "c", "cpp", "go", "python", "lua", "vim", "vimdoc", "cmake"},
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+            enable = true,               -- 启用 Treesitter 语法高亮
+            additional_vim_regex_highlighting = false, -- 避免与正则高亮冲突
+        },
+        indent = {
+            enable = true,               -- 启用基于语法的智能缩进
+        },
+    })
+end
